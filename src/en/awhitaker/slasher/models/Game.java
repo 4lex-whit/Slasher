773eaf8b85e02dcd7d342c6238f76ceb3942d0d5
BuildTools <unconfigured@null.spigotmc.org>
2024-01-07 20:06:09 +0000

    Add main comments to start procedure

--- src/en/awhitaker/slasher/models/Game.java
+++ src/en/awhitaker/slasher/models/Game.java
@@ -2,6 +2,12 @@ package en.awhitaker.slasher.models;
 
 public class Game {
 	public static void start() {
+		// prepare map
 		
+		// prepare players
+		
+		// release players
+		
+		// end game
 	}
 }
