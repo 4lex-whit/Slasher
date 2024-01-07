0fc19f35c0ee01e2727c2cc7f2fae90db4c2361a
BuildTools <unconfigured@null.spigotmc.org>
2024-01-07 20:09:24 +0000

    Declare Game class variables

--- src/en/awhitaker/slasher/models/Game.java
+++ src/en/awhitaker/slasher/models/Game.java
@@ -1,6 +1,15 @@
 package en.awhitaker.slasher.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.entity.Player;
+
 public class Game {
+	private static List<Player> players = new ArrayList<Player>();
+	private static Player slasher;
+	private static List<Player> survivors = new ArrayList<Player>();
+	
 	public static void start() {
 		// prepare map
 		
