eb73c95d3b4d9a61db3b7a40facf159508f4d9d2
BuildTools <unconfigured@null.spigotmc.org>
2024-01-08 21:07:12 +0000

    Register slasher command in onEnable procedure

--- src/en/awhitaker/slasher/Slasher.java
+++ src/en/awhitaker/slasher/Slasher.java
@@ -2,9 +2,11 @@ package en.awhitaker.slasher;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import en.awhitaker.slasher.commands.SlasherCommand;
+
 public class Slasher extends JavaPlugin {
 	@Override
 	public void onEnable() {
-		
+		this.getCommand("slasher").setExecutor(new SlasherCommand());
 	}
 }
