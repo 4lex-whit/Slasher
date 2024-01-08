1833cad755a48ef755a71900816f302a894c6aac
BuildTools <unconfigured@null.spigotmc.org>
2024-01-08 20:54:33 +0000

    Check sender in slasher command

--- src/en/awhitaker/slasher/commands/SlasherCommand.java
+++ src/en/awhitaker/slasher/commands/SlasherCommand.java
@@ -3,10 +3,17 @@ package en.awhitaker.slasher.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class SlasherCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		// check sender
+		if (!(sender instanceof Player)) {
+			sender.sendMessage("Only players can use this command.");
+			return true;
+		}
+		
 		return false;
 	}
 }
