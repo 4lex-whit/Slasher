031f5b4479be10bdb23cdda3b6029922a33f3980
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-05 22:42:57 +0000

    Add constructor to ArenaDataManager class

--- src/main/java/en/awhitaker/slasher/filemanagers/ArenaDataManager.java
+++ src/main/java/en/awhitaker/slasher/filemanagers/ArenaDataManager.java
@@ -1,5 +1,11 @@
 package en.awhitaker.slasher.filemanagers;
 
+import en.awhitaker.slasher.Slasher;
+
 public class ArenaDataManager {
+	private Slasher plugin;
 	
+	public ArenaDataManager(Slasher plugin) {
+		this.plugin = plugin;
+	}
 }
