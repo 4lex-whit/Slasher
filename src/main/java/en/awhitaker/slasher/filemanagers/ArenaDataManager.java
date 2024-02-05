f2a847382591d3d66c086fc91dba06079b44d087
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-05 22:46:31 +0000

    Add ArenaDataConfig class variables

--- src/main/java/en/awhitaker/slasher/filemanagers/ArenaDataManager.java
+++ src/main/java/en/awhitaker/slasher/filemanagers/ArenaDataManager.java
@@ -1,9 +1,16 @@
 package en.awhitaker.slasher.filemanagers;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+
 import en.awhitaker.slasher.Slasher;
 
 public class ArenaDataManager {
 	private Slasher plugin;
+	
+	private FileConfiguration arenaDataConfig = null;
+	private File configFile = null;
 	
 	public ArenaDataManager(Slasher plugin) {
 		this.plugin = plugin;
