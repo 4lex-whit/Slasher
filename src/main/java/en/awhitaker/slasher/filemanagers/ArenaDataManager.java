c7a44fc5cf0fe31017a7ad7c5c8d7c9107c483a7
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-05 22:51:59 +0000

    Set class variables in reload procedure

--- src/main/java/en/awhitaker/slasher/filemanagers/ArenaDataManager.java
+++ src/main/java/en/awhitaker/slasher/filemanagers/ArenaDataManager.java
@@ -3,6 +3,7 @@ package en.awhitaker.slasher.filemanagers;
 import java.io.File;
 
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 
 import en.awhitaker.slasher.Slasher;
 
@@ -17,6 +18,10 @@ public class ArenaDataManager {
 	}
 	
 	public void reload() {
+		// check & set configFile and arenaDataConfig
+		if (configFile == null)
+			configFile = new File(plugin.getDataFolder(), "arena-data.yml");
 		
+		arenaDataConfig = YamlConfiguration.loadConfiguration(configFile);
 	}
 }
