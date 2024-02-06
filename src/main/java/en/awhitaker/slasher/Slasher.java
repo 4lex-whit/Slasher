06e2205fdf4d5caeeee00a3bd6af67d698446418
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-06 00:02:25 +0000

    Declare arenaDataManager in Slasher class

--- src/main/java/en/awhitaker/slasher/Slasher.java
+++ src/main/java/en/awhitaker/slasher/Slasher.java
@@ -2,7 +2,11 @@ package en.awhitaker.slasher;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import en.awhitaker.slasher.filemanagers.ArenaDataManager;
+
 public class Slasher extends JavaPlugin {
+	public ArenaDataManager arenaDataManager;
+	
 	@Override
 	public void onEnable() {
 		
