2d698647152c97f5135355226518ff5b7be40116
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-16 17:05:01 +0000

    Add Slasher instance to PlayerMoveListener class

--- src/main/java/en/awhitaker/slasher/listeners/PlayerMoveListener.java
+++ src/main/java/en/awhitaker/slasher/listeners/PlayerMoveListener.java
@@ -4,7 +4,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import en.awhitaker.slasher.Slasher;
+
 public class PlayerMoveListener implements Listener {
+	private Slasher plugin;
+	public PlayerMoveListener(Slasher plugin) {
+		this.plugin = plugin;
+	}
+	
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
 		
