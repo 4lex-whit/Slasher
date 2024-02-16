e1a8d78dc3b553df767a599f6cfa73a44da8f71e
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-16 17:34:37 +0000

    Cancel event if frozen in PlayerMoveListener class

--- src/main/java/en/awhitaker/slasher/listeners/PlayerMoveListener.java
+++ src/main/java/en/awhitaker/slasher/listeners/PlayerMoveListener.java
@@ -1,10 +1,12 @@
 package en.awhitaker.slasher.listeners;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import en.awhitaker.slasher.Slasher;
+import en.awhitaker.slasher.models.Game;
 
 public class PlayerMoveListener implements Listener {
 	private Slasher plugin;
@@ -14,6 +16,11 @@ public class PlayerMoveListener implements Listener {
 	
 	@EventHandler
 	public void onPlayerMove(PlayerMoveEvent event) {
+		Player player = event.getPlayer();
 		
+		// check player
+		if ((player.getUniqueId().equals(Game.getSlasherId()) && Game.isSlasherFrozen()) || (Game.getSurvivorIds().contains(player.getUniqueId()) && Game.isSurvivorsFrozen())) {
+			event.setCancelled(true);
+		}
 	}
 }
