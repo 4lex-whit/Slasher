3a0380b0a06a8fb641082b9518041807f6627a7f
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-16 18:20:54 +0000

    Remove player from game in onPlayerQuit procedure

--- src/main/java/en/awhitaker/slasher/listeners/PlayerQuitListener.java
+++ src/main/java/en/awhitaker/slasher/listeners/PlayerQuitListener.java
@@ -1,12 +1,20 @@
 package en.awhitaker.slasher.listeners;
 
+import java.util.UUID;
+
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
+import en.awhitaker.slasher.models.Game;
+
 public class PlayerQuitListener implements Listener {
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event) {
+		UUID uuid = event.getPlayer().getUniqueId();
 		
+		// check if player is playing
+		if (Game.getPlayerIds().contains(uuid))
+			Game.removePlayer(uuid);
 	}
 }
