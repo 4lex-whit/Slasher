22d1fe7be5e03d87490d0c0262363472e9954995
alex_whit <110124629+4lex-whit@users.noreply.github.com>
2024-02-29 22:53:14 +0000

    Add isPlayerPlaying function

--- src/main/java/en/awhitaker/slasher/utils/PlayerUtil.java
+++ src/main/java/en/awhitaker/slasher/utils/PlayerUtil.java
@@ -1,5 +1,19 @@
 package en.awhitaker.slasher.utils;
 
+import java.util.UUID;
+
+import en.awhitaker.slasher.models.Game;
+
 public class PlayerUtil {
-	
+	/**
+	 * Checks if the specified player is in the Slasher game.
+	 * @param uuid the {@link java.util.UUID UUID} of the player
+	 * @return {@link true} if the player is playing<br/>
+	 *         {@code false} otherwise
+	 */
+	public boolean isPlayerPlaying(UUID uuid) {
+		if (Game.getPlayerIds().contains(uuid))
+			return true;
+		return false;
+	}
 }
